271b0469a359e47f33e02b09e4803fc84857d728
rabab-1993 <Rabab.Abdulaziz.elq.js@tuwaiq.edu.sa>
2021-10-20 10:22:58 +0300

    updated

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const body1 = document.querySelector("body");
 let heading = document.createElement("h1");
 heading.textContent = "To do list";
-body1.append(heading);
 let ul = document.createElement("ul");
+body1.append(heading, ul);
+
+let todos = ['wake up', 'eat breakfast', 'code'];
+ 
+let renderList = () => {
+   
+    for (let index = 0; index < todos.length; index++) {
+        let list = document.createElement("li");
+        list.textContent = `${todos[index]}`;
+        ul.appendChild(list);
+    }
+}
+
+renderList()
+
