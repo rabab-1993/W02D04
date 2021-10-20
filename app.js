167d8355c880a34052576fab1dfa66bb2b27a215
rabab-1993 <Rabab.Abdulaziz.elq.js@tuwaiq.edu.sa>
2021-10-20 16:36:44 +0300

    updated

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
+// varibals
+
 const body1 = document.querySelector("body");
+let todos = ["wake up", "eat breakfast", "code", "sleeping"];
+let delbtn = document.querySelector('.del');
+
+// creating h1
+
 let heading = document.createElement("h1");
 heading.textContent = "To do list";
-let ul = document.createElement("ul");
-body1.append(heading, ul);
-
-// creat list
-let todos = ['wake up', 'eat breakfast', 'code'];
-let renderList = () => {
-    for (let index = 0; index < todos.length; index++) {
-        let list = document.createElement("li");
-        list.textContent = `${todos[index]}`;
-        ul.appendChild(list);
-    }
-}
-
-renderList()
 
 // creat input and buttons
 let input = document.createElement("input");
 let bttn = document.createElement("button");
 bttn.textContent = "Add";
-body1.append(input, bttn)
-// addToList
-bttn.addEventListener("click", () => {
-    todos.push(input.value);
-    input.value = ""
-    console.log(todos);
-})
 
-// delete List Item
-// let deleteListItem = () =>{
-//     let deleteBttn = document.createElement("button");
-//     deleteBttn.textContent ="delete";
-//     list.append(deleteBttn)
-//     deleteBttn.addEventListener("click", deleteListItem)
+// creat ul
+let ul = document.createElement("ul");
+body1.append(heading, input, bttn, ul);
+
+// creat list
+let renderList = () => {
+  for (let index = 0; index < todos.length; index++) {
+    let list = document.createElement("li");
+    list.textContent = `${todos[index]}`;
+    ul.appendChild(list);
+    let deleteBttn = document.createElement("button");
+    deleteBttn.textContent = "delete";
+    deleteBttn.className = "del";
+    list.append(deleteBttn);
+}
+};
+renderList();
+
+// addToList
+let addToList = (on) => {
+  todos.push(input.value);
+  input.value = "";
+  console.log(todos);
+
+  renderList();
+};
+
+ // delete List Item
+ let deleteListItem = () => {
     
-// }
+    
+  };
+ 
+
+bttn.addEventListener("click", addToList);
