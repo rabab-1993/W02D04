81628948a33e3cb139135cbcead3bc2b43417f33
rabab-1993 <Rabab.Abdulaziz.elq.js@tuwaiq.edu.sa>
2021-10-20 14:07:25 +0300

    updated

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ heading.textContent = "To do list";
 let ul = document.createElement("ul");
 body1.append(heading, ul);
 
+// creat list
 let todos = ['wake up', 'eat breakfast', 'code'];
- 
 let renderList = () => {
-   
     for (let index = 0; index < todos.length; index++) {
         let list = document.createElement("li");
         list.textContent = `${todos[index]}`;
@@ -17,3 +16,23 @@ let renderList = () => {
 
 renderList()
 
+// creat input and buttons
+let input = document.createElement("input");
+let bttn = document.createElement("button");
+bttn.textContent = "Add";
+body1.append(input, bttn)
+// addToList
+bttn.addEventListener("click", () => {
+    todos.push(input.value);
+    input.value = ""
+    console.log(todos);
+})
+
+// delete List Item
+// let deleteListItem = () =>{
+//     let deleteBttn = document.createElement("button");
+//     deleteBttn.textContent ="delete";
+//     list.append(deleteBttn)
+//     deleteBttn.addEventListener("click", deleteListItem)
+    
+// }
