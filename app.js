dcb4284adeb489eb9a8c551d2c909cb1b500ba33
rabab-1993 <Rabab.Abdulaziz.elq.js@tuwaiq.edu.sa>
2021-10-20 09:42:39 +0300

    added file

---
+++ app.js
@@ -0,0 +1,5 @@
+const body1 = document.querySelector("body");
+let heading = document.createElement("h1");
+heading.textContent = "To do list";
+body1.append(heading);
+let ul = document.createElement("ul");
